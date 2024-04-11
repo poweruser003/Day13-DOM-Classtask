f2139fbf7583caf8b4c74703af003ccc5ae8e7a3
poweruser003 <aadhithya.cloud003@gmail.com>
2024-04-11 16:34:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// let div= document.createElement("div")
+// console.log(div)
+// div.innerHTML= "<p> this is a div </p> "
+// document.body.append(div)
+
+
+
+// var  container= document.createElement("div")
+// container.className= "container"
+
+// var row= document.createElement("div")
+// row.className= "row"
+
+// var col= document.createElement("div")
+// col.className= "col"
+// col.innerHTML="this is col tag"
+
+//  row.append(col)
+//  container.append(row)
+//  document.body.append(container)
+
+
+
+// let res= document.getElementById("main").innerHTML= "modified by js"
+// console.log(res)
+function foo(){
+    let fname= document.getElementById("FirstName").value;
+    let mname= document.getElementById("MiddleName").value;
+    let lname= document.getElementById("LastName").value;
+    let email= document.getElementById("email").value;
+    let addr= document.getElementById("address").value;
+    console.log(fname);
+    console.log(mname);
+    console.log(lname);
+    console.log(email);
+    console.log(addr);
+}
